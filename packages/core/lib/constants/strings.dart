9e28f44d1f25bc8a35b6603c5d129c30a38818f4
Simon Pham <simonpham.dn@gmail.com>
2025-11-08 19:58:48 +0700

    🍱 Add JetBrainsMono font

--- packages/core/lib/constants/strings.dart
+++ packages/core/lib/constants/strings.dart
@@ -2,6 +2,7 @@ import 'package:platform_utils/platform_utils.dart';
 
 const String kAppFontFamily = 'Nunito';
 const String kMonoFontFamily = 'Iosevka';
+const String kCodeFontFamily = 'JetBrainsMono';
 
 const String kDefaultLanguage = 'en';
 const kSupportedLanguages = {
