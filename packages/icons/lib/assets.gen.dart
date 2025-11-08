4423a9346f7689c62f888c8f85b4b8117b8914f9
Simon Pham <simonpham.dn@gmail.com>
2025-11-08 23:40:43 +0700

    🍱 Add new icons

--- packages/icons/lib/assets.gen.dart
+++ packages/icons/lib/assets.gen.dart
@@ -12,93 +12,24 @@
 class Assets {
   const Assets._();
 
-  static const String add01 = 'assets/add-01.svg';
-  static const String arrowRight01Round = 'assets/arrow-right-01-round.svg';
-  static const String arrowTurnBackwardRound =
-      'assets/arrow-turn-backward-round.svg';
-  static const String cancel01 = 'assets/cancel-01.svg';
-  static const String customize = 'assets/customize.svg';
-  static const String delete01 = 'assets/delete-01.svg';
-  static const String delete04 = 'assets/delete-04.svg';
-  static const String deleteThrow = 'assets/delete-throw.svg';
-  static const String dollarCircle = 'assets/dollar-circle.svg';
-  static const String edit02 = 'assets/edit-02.svg';
-  static const String file01 = 'assets/file-01.svg';
-  static const String file02 = 'assets/file-02.svg';
-  static const String fileAddBulk = 'assets/file-add-bulk.svg';
-  static const String fileAudio = 'assets/file-audio.svg';
-  static const String fileBlock = 'assets/file-block.svg';
-  static const String fileExport = 'assets/file-export.svg';
-  static const String fileVideo = 'assets/file-video.svg';
-  static const String flash = 'assets/flash.svg';
+  static const String archive01 = 'assets/archive-01.svg';
+  static const String bookOpen01 = 'assets/book-open-01.svg';
+  static const String bookOpen02 = 'assets/book-open-02.svg';
   static const String folder01 = 'assets/folder-01.svg';
-  static const String folderOpen = 'assets/folder-open.svg';
-  static const String globe = 'assets/globe.svg';
-  static const String helpCircle = 'assets/help-circle.svg';
-  static const String image01 = 'assets/image-01.svg';
-  static const String layers01StrokeRounded =
-      'assets/layers-01-stroke-rounded.svg';
-  static const String layersLogoStrokeRounded =
-      'assets/layers-logo-stroke-rounded.svg';
-  static const String legalDocument01 = 'assets/legal-document-01.svg';
-  static const String message01 = 'assets/message-01.svg';
-  static const String moreVertical = 'assets/more-vertical.svg';
-  static const String note01 = 'assets/note-01.svg';
-  static const String notification02 = 'assets/notification-02.svg';
-  static const String reload = 'assets/reload.svg';
-  static const String securityLock = 'assets/security-lock.svg';
-  static const String setting01 = 'assets/setting-01.svg';
-  static const String setup02 = 'assets/setup-02.svg';
-  static const String share01 = 'assets/share-01.svg';
-  static const String share05 = 'assets/share-05.svg';
-  static const String smileBulk = 'assets/smile-bulk.svg';
-  static const String sorting01 = 'assets/sorting-01.svg';
-  static const String stop = 'assets/stop.svg';
-  static const String tick02 = 'assets/tick-02.svg';
-  static const String verticalDragDrop = 'assets/vertical-drag-&-drop.svg';
+  static const String folder02 = 'assets/folder-02.svg';
+  static const String hierarchyFiles = 'assets/hierarchy-files.svg';
+  static const String messageProgramming = 'assets/message-programming.svg';
+  static const String terminal = 'assets/terminal.svg';
 
   /// List of all assets
   static List<String> get values => [
-    add01,
-    arrowRight01Round,
-    arrowTurnBackwardRound,
-    cancel01,
-    customize,
-    delete01,
-    delete04,
-    deleteThrow,
-    dollarCircle,
-    edit02,
-    file01,
-    file02,
-    fileAddBulk,
-    fileAudio,
-    fileBlock,
-    fileExport,
-    fileVideo,
-    flash,
+    archive01,
+    bookOpen01,
+    bookOpen02,
     folder01,
-    folderOpen,
-    globe,
-    helpCircle,
-    image01,
-    layers01StrokeRounded,
-    layersLogoStrokeRounded,
-    legalDocument01,
-    message01,
-    moreVertical,
-    note01,
-    notification02,
-    reload,
-    securityLock,
-    setting01,
-    setup02,
-    share01,
-    share05,
-    smileBulk,
-    sorting01,
-    stop,
-    tick02,
-    verticalDragDrop,
+    folder02,
+    hierarchyFiles,
+    messageProgramming,
+    terminal,
   ];
 }
