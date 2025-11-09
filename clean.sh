807f5bd3d36334cd83d29cc56c9b67e610fe8cc1
Simon Pham <simonpham.dn@gmail.com>
2025-11-09 18:17:45 +0700

    🔨 Add new packages to clean scripts

--- clean.sh
+++ clean.sh
@@ -2,9 +2,15 @@ packages=(
   "apps/simon_web"
   "modules/simon"
   "packages/core"
+  "packages/core_remote_data"
+  "packages/core_remote_data_pb"
+  "packages/design_system"
   "packages/icons"
+  "packages/ide_layout"
   "packages/l10n"
   "packages/platform_utils"
+  "packages/ui_file_tree"
+  "packages/ui_markdown"
   "packages/utils"
 )
 
