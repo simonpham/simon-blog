b06b8dcbd0799acc3b887d3c169f71cbbfc164ce
Simon Pham <simonpham.dn@gmail.com>
2025-11-13 23:00:25 +0700

    ✨ Setup default location

--- apps/simon_web/lib/router.dart
+++ apps/simon_web/lib/router.dart
@@ -1,9 +1,21 @@
 import 'package:core/core.dart';
 import 'package:simon/simon.dart';
 
+const _defaultLocation = '/welcome.md';
+
 final GoRouter kAppRouter = GoRouter(
-  initialLocation: HomePage.routePath,
+  initialLocation: _defaultLocation,
   errorBuilder: (_, _) => const ErrorPage(),
+  redirect: (context, state) {
+    final postId = switch (state.pathParameters) {
+      {HomePage.identifierParam: String identifier} => identifier,
+      _ => null,
+    };
+    if (postId == null) {
+      return _defaultLocation;
+    }
+    return null;
+  },
   routes: [
     ...kSimonRoutes,
   ],
